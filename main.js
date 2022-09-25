79c44aa36f10d9de80139d02c0c60e3795f1e931
Nick Wilcox <Nickwilcox001@gmail.com>
2022-09-25 12:40:49 -0400

    Finished the webstie with name capitalizer

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const customerContainer = document.querySelector("#customerContainer")
 
+function capName (name)
+{
+    let name2 = ""
+    name2 = name.charAt(0).toUpperCase() + name.slice(1);
+    return name2;
+}
+
+
 function buildDirectory(customerArray)
 {
     for (let customer of customerArray)
@@ -10,19 +18,25 @@ function buildDirectory(customerArray)
         let email = document.createElement('p')
         let address = document.createElement('p')
         let DOB = document.createElement('p')
+        let customerDate = document.createElement('p')
         img.classList.add("thumbnail")
+        name.classList.add("name")
+
+
 
         img.src = `${customer.picture.large}`
-        name.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
+        name.innerText = `${capName(customer.name.first)} ${capName(customer.name.last)}`;
         email.innerText = `${customer.email}`
-        //DOB.innerText = `DOB: ${customer.dob.date.format("MMM Do YYYY")}`
-        //address.innerText = `${customer.location.street.number}` `${customer.location.street.name}` `${customer.location.city}``${customer.location.state}`
+        address.innerText = `${customer.location.street.number} ${customer.location.street.name} ${customer.location.city}, ${nameToAbbr(customer.location.state)} ${customer.location.postcode}`
+        DOB.innerText = `DOB: ${moment(customer.dob.date).format("MMM Do YYYY")}`
+        customerDate.innerText = `Customer Since: ${moment(customer.registered.date).format("MMM Do YYYY")}`
         
         customerDiv.appendChild(img);
         customerDiv.appendChild(name);
         customerDiv.appendChild(email)
         customerDiv.appendChild(address)
         customerDiv.appendChild(DOB)
+        customerDiv.appendChild(customerDate)
         
         customerContainer.appendChild(customerDiv);
         customerDiv.classList.add("customer")
